ae7fdcd7a32b61a33e5ac5495bf620c0f7dd9822
William Ramos de Assis Rezende <wrassis84@gmail.com>
2023-06-07 16:25:47 -0300

    feat: add vpc and subnet_id for terraform resources

--- output.tf
+++ output.tf
@@ -6,3 +6,12 @@ output "instance_public_ip" {
 description = "Public IP address of the EC2 instance"
 value       = aws_instance.server.public_ip
 }
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.server.id
+}
+
+output "subnet_id" {
+  description = "ID of the VPC subnet"
+  value       = aws_subnet.server.id
+}
