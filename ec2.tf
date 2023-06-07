48c5547f00a017925936aed502c50b55555c9e19
William Ramos de Assis Rezende <wrassis84@gmail.com>
2023-06-07 16:43:11 -0300

    feat: add resources for vpc and subnet_id

--- ec2.tf
+++ ec2.tf
@@ -8,3 +8,23 @@ resource "aws_instance" "server" {
     Provisioner = "Terraform"
   }
 }
+
+resource "aws_vpc" "server" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "server" {
+  vpc_id                  = aws_vpc.server.id
+  cidr_block              = var.subnet_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone       = var.aws_az
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
